2e76b4cf4e5ea2520c8ad6592e802e2055c344da
shivamnoida <pandit7805@gmail.com>
2024-11-16 17:50:45 +0530

    inital commit

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      sbjdsbjdsjsks
+      djdkjdkjdkjd
+      djdkjdkjdkjddkdhdkhd
+      dbjdhdhdkjhdhdjdhjdh
+      hddhdjhhd
+      ndhdjhd
+    </div>
+  )
+}
+
+export default About
